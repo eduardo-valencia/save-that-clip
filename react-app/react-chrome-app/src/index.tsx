bf9eecc0ea5a243f3205d21e1a779f971dbffbe0
EDUARDO VALENCIA <8025918@gmail.com>
2023-04-05 15:56:31 -0700

    Add initial files

---
+++ react-app/react-chrome-app/src/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+
+let rootElement: HTMLElement
+
+const createRoot = (): HTMLElement => {
+  const rootElement = document.createElement('div')
+  rootElement.id = 'save-that-clip'
+  document.body.appendChild(rootElement)
+  return rootElement
+}
+
+const createStyles = (): void => {
+  const element = document.createElement('style')
+  rootElement.appendChild(element)
+}
+
+rootElement = createRoot()
+createStyles()
+const root = ReactDOM.createRoot(rootElement)
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
