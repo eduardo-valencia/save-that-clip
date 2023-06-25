6979c5ea1d62d766aa8a4ea3dfdbc1f68b73f766
EDUARDO VALENCIA <8025918@gmail.com>
2023-06-24 18:06:28 -0700

    Use RegEx to test extension has correct title

--- e2e/tests/main.spec.ts
+++ e2e/tests/main.spec.ts
@@ -1,8 +1,8 @@
 import { Locator } from '@playwright/test'
 import { test, expect } from './fixture'
 
-test('popup page', async ({ page, extensionId }) => {
+test('Has the correct title', async ({ page, extensionId }) => {
   await page.goto(`chrome-extension://${extensionId}/popup.html`)
   const body: Locator = page.locator('body')
-  expect(body).toHaveText('Bookmarks')
+  expect(body).toHaveText(/Bookmarks/)
 })
