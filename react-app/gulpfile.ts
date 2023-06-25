fb0563533ee83c3c255bfd1f83def305ba0e4475
EDUARDO VALENCIA <8025918@gmail.com>
2023-06-24 19:31:43 -0700

    Reload after webpack is done, fix not building app

--- react-app/gulpfile.ts
+++ react-app/gulpfile.ts
@@ -1,13 +1,31 @@
 import gulp from 'gulp'
 import webpack from 'webpack-stream'
-import webpackConfig from './webpack.config'
+import open from 'open'
 import { Configuration } from 'webpack'
+import { ChildProcess } from 'child_process'
 
-const watchWithWebpack = () => {
+import webpackConfig from './webpack.config'
+
+/**
+ * Must install
+ * https://chrome.google.com/webstore/detail/extensions-reloader/fimgfedafeadlieiabdeeaodndnlbhid
+ * for this to work.
+ */
+const reloadExtension = (): Promise<ChildProcess> => {
+  return open('http://reload.extensions')
+}
+
+/**
+ * We separate this from the other watcher because it helps with performance.
+ */
+const watchWithWebpack = (): NodeJS.ReadWriteStream => {
   const configWithType = webpackConfig as Configuration
   return gulp
     .src('./src/index.tsx')
-    .pipe(webpack({ watch: true, ...configWithType }))
+    .pipe(
+      webpack({ watch: true, ...configWithType }, undefined, reloadExtension)
+    )
+    .pipe(gulp.dest('../extension'))
 }
 
 export const watch = watchWithWebpack
