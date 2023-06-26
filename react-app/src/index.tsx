1a1fc27891eb7376e3a6da8cbc64042d8f554a65
EDUARDO VALENCIA <8025918@gmail.com>
2023-06-25 19:18:45 -0700

    Using const instead of let in index.tsx

--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -1,35 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import CssBaseline from '@mui/material/CssBaseline'
-import reportWebVitals from './reportWebVitals'
-
-let rootElement: HTMLElement
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import CssBaseline from "@mui/material/CssBaseline";
+import reportWebVitals from "./reportWebVitals";
 
 const createRoot = (): HTMLElement => {
-  const rootElement = document.createElement('div')
-  rootElement.id = 'save-that-clip'
-  document.body.appendChild(rootElement)
-  return rootElement
-}
+  const rootElement = document.createElement("div");
+  rootElement.id = "save-that-clip";
+  document.body.appendChild(rootElement);
+  return rootElement;
+};
 
 const createStyles = (): void => {
-  const element = document.createElement('style')
-  rootElement.appendChild(element)
-}
+  const element = document.createElement("style");
+  rootElement.appendChild(element);
+};
 
-rootElement = createRoot()
-createStyles()
-const root = ReactDOM.createRoot(rootElement)
+const rootElement: HTMLElement = createRoot();
+createStyles();
+const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
     <CssBaseline />
     <App />
   </React.StrictMode>
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
