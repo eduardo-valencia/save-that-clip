2cff92cc049854e863b95a59579ca0d057f1e6bd
EDUARDO VALENCIA <8025918@gmail.com>
2023-06-28 18:38:57 -0700

    Avoid using process because it's not allowed in extension

--- react-app/src/config.ts
+++ react-app/src/config.ts
@@ -1,21 +1,9 @@
-const { REACT_APP_API_URL, NODE_ENV } = process.env;
-
-const handleInvalidApiUrl = (): never => {
-  throw new Error("The API's URL is not a string.");
-};
-
-const getAndValidateApiUrl = (): string => {
-  if (NODE_ENV === "test") return "http://localhost:5000";
-  if (typeof REACT_APP_API_URL !== "string") return handleInvalidApiUrl();
-  return REACT_APP_API_URL;
-};
-
 export interface Config {
   apiUrl: string;
 }
 
 const config: Config = {
-  apiUrl: getAndValidateApiUrl(),
+  apiUrl: "http://localhost:5000",
 };
 
 export default config;
