b6feb0e9bb9d88c2568f06f94ba5f8388d28b530
EDUARDO VALENCIA <8025918@gmail.com>
2023-06-28 17:48:13 -0700

    Update api URL env var's name

--- react-app/src/config.ts
+++ react-app/src/config.ts
@@ -1,4 +1,4 @@
-const { API_URL, NODE_ENV } = process.env;
+const { REACT_APP_API_URL, NODE_ENV } = process.env;
 
 const handleInvalidApiUrl = (): never => {
   throw new Error("The API's URL is not a string.");
@@ -6,8 +6,8 @@ const handleInvalidApiUrl = (): never => {
 
 const getAndValidateApiUrl = (): string => {
   if (NODE_ENV === "test") return "http://localhost:5000";
-  if (typeof API_URL !== "string") return handleInvalidApiUrl();
-  return API_URL;
+  if (typeof REACT_APP_API_URL !== "string") return handleInvalidApiUrl();
+  return REACT_APP_API_URL;
 };
 
 export interface Config {
