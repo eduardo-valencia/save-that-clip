9f54d61523b07fa49d25f32b9b7fb20128b604d9
EDUARDO VALENCIA <8025918@gmail.com>
2023-07-03 14:36:03 -0700

    Reset query when clicking on a series's back button

--- extension/popup/src/pages/Series/ToolbarWithBackButton.tsx
+++ extension/popup/src/pages/Series/ToolbarWithBackButton.tsx
@@ -1,12 +1,19 @@
 import { Box, Button } from "@mui/material";
 import LeftArrowIcon from "@mui/icons-material/ArrowBackIos";
-import React from "react";
+import React, { useContext } from "react";
 import { NavigateFunction, useNavigate } from "react-router-dom";
+import {
+  SearchContext,
+  SearchContextValue,
+} from "../../components/SearchProvider";
 
 export default function ToolbarWithBackButton() {
   const navigate: NavigateFunction = useNavigate();
+  const { setQuery }: SearchContextValue = useContext(SearchContext);
 
   const handleClick = (): void => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    setQuery!("");
     navigate("/");
   };
 
