031bd94373f2e253b593c70897357fe6c6275532
EDUARDO VALENCIA <8025918@gmail.com>
2023-07-02 18:57:25 -0700

    Enable deleting bookmarks

--- extension/popup/src/components/BookmarksList/BookmarkItem/DeleteBookmarkButton.tsx
+++ extension/popup/src/components/BookmarksList/BookmarkItem/DeleteBookmarkButton.tsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Bookmark } from "../../../bookmarks/Bookmarks.repo-abstraction";
 import { Button } from "@mui/material";
+import {
+  BookmarksContext,
+  BookmarksContextValue,
+} from "../../BookmarksProvider";
+import { BookmarksService } from "../../../bookmarks/Bookmarks.service";
 
 interface Props {
   bookmarkId: Bookmark["id"];
 }
 
-export default function DeleteBookmarkButton(props: Props) {
+export default function DeleteBookmarkButton({ bookmarkId }: Props) {
+  const { findAndSetBookmarks }: BookmarksContextValue =
+    useContext(BookmarksContext);
+
+  const bookmarksService = new BookmarksService();
+
+  const deleteBookmarkAndRefreshBookmarks = async (): Promise<void> => {
+    await bookmarksService.destroy(bookmarkId);
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    await findAndSetBookmarks!();
+  };
+
+  const handleClick = (): void => {
+    void deleteBookmarkAndRefreshBookmarks();
+  };
+
   return (
     <Button
       variant="contained"
@@ -18,6 +38,7 @@ export default function DeleteBookmarkButton(props: Props) {
           backgroundColor: "rgba(255, 0, 0, 0.3)",
         },
       }}
+      onClick={handleClick}
     >
       Delete
     </Button>
