d443ebf729f2ade797813a5bd4e19324a427b426
EDUARDO VALENCIA <8025918@gmail.com>
2023-07-03 18:17:00 -0700

    Add Sentry to popup's Webpack

--- extension/popup/webpack.config.js
+++ extension/popup/webpack.config.js
@@ -1,6 +1,26 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
 const commonConfig = require("../main/common/common-webpack-config");
+const { sentryWebpackPlugin } = require("@sentry/webpack-plugin");
+
+const { SENTRY_ORG, SENTRY_PROJECT, SENTRY_AUTH_TOKEN, NODE_ENV } = process.env;
+
+const getSentryPlugin = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return sentryWebpackPlugin({
+    org: SENTRY_ORG,
+    project: SENTRY_PROJECT,
+    authToken: SENTRY_AUTH_TOKEN,
+  });
+};
+
+const getPlugins = () => {
+  if (NODE_ENV === "production") {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return [getSentryPlugin()];
+  }
+  return [];
+};
 
 const buildFolder = path.resolve(__dirname, "../build");
 
@@ -14,4 +34,6 @@ module.exports = {
   resolve: {
     extensions: [...commonConfig.resolve.extensions, ".js", ".tsx"],
   },
+  devtool: "source-map",
+  plugins: getPlugins(),
 };
