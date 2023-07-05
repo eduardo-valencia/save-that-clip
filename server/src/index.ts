4e54382afc69f220f6a743cd889c21ac890191a3
EDUARDO VALENCIA <8025918@gmail.com>
2023-07-04 17:52:26 -0700

    Handle Axios errors in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,45 @@
 import express, { Express, Request, Response } from 'express'
 import cors from 'cors'
-import axios, { AxiosResponse } from 'axios'
+import axios, { AxiosError, AxiosResponse } from 'axios'
 
 const app: Express = express()
 
 app.use(cors())
 
-const proxyToNetflix = async (req: Request, res: Response): Promise<void> => {
+const requestNetflixAndSendResponse = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   const netflixUrl: string = `http://netflix.com${req.path}`
   const { data, status }: AxiosResponse = await axios.get(netflixUrl)
   res.status(status).send(data)
 }
 
-app.get('*', proxyToNetflix)
+const sendDefaultError = (res: Response, status: number = 500): void => {
+  res
+    .status(status)
+    .send({ message: 'Sorry, there was a problem. Please try again later.' })
+}
+
+const handleProxyError = (error: unknown, res: Response): void => {
+  console.error(error)
+  if (error instanceof AxiosError)
+    return sendDefaultError(res, error.response?.status)
+  return sendDefaultError(res)
+}
+
+const tryToProxyToNetflix = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    await requestNetflixAndSendResponse(req, res)
+  } catch (error) {
+    handleProxyError(error, res)
+  }
+}
+
+app.get('*', tryToProxyToNetflix)
 
 const portWithType = process.env.PORT as number | undefined
 const port: number = portWithType || 5000
