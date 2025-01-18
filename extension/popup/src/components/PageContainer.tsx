2ccc3745d34806c03d87ade0d6856668668f0ba8
EDUARDO VALENCIA <8025918@gmail.com>
2025-01-17 18:52:50 -0800

    Hide scrollbars

--- extension/popup/src/components/PageContainer.tsx
+++ extension/popup/src/components/PageContainer.tsx
@@ -1,5 +1,9 @@
 import { Box, BoxProps } from "@mui/material";
 
+/**
+ * Defines the padding for the page. Note that this is also used in certain
+ * components, so we shouldn't put styles like overflow styles  here.
+ */
 export default function PageContainer({ sx, ...other }: BoxProps) {
   return <Box sx={{ px: "1.5rem", ...sx }} {...other} />;
 }
