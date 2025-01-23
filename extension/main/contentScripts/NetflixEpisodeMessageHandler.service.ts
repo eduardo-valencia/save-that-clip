6ef576b9454073ace90212f2efce67afabd3c60b
EDUARDO VALENCIA <8025918@gmail.com>
2025-01-22 18:57:39 -0800

    add init service to get episode info

--- extension/main/contentScripts/NetflixEpisodeMessageHandler.service.ts
+++ extension/main/contentScripts/NetflixEpisodeMessageHandler.service.ts
@@ -1,12 +1,61 @@
 import { EpisodeTime, PossibleEpisodeTime } from "../common/messages";
+import pWaitFor from "p-wait-for";
+
+type EpisodeName = string | null;
+export interface NetflixEpisodeInfo {
+  timeInMs: PossibleEpisodeTime;
+  episodeName: EpisodeName;
+}
+
+type PossibleVideo = HTMLVideoElement | null;
 
 export class NetflixEpisodeMessageHandlers {
   private getVideoTimeInMs = (video: HTMLVideoElement): EpisodeTime => {
     return video.currentTime * 1000;
   };
 
-  public getEpisodeInfo = (): PossibleEpisodeTime => {
-    const video: HTMLVideoElement | null = document.querySelector("video");
+  private queryVideo = (): PossibleVideo => {
+    return document.querySelector("video");
+  };
+
+  private getEpisodeTime = (): PossibleEpisodeTime => {
+    const video: PossibleVideo = this.queryVideo();
     return video ? this.getVideoTimeInMs(video) : null;
+  };
+
+  private findEpisodeName = (): NetflixEpisodeInfo["episodeName"] => {
+    const toolbar: HTMLElement | null = document.querySelector(
+      '[data-uia="video-title"] h4'
+    );
+    return toolbar ? toolbar.innerText : null;
+  };
+
+  private getIfEpisodeNameFound = (): boolean => {
+    const name: EpisodeName = this.findEpisodeName();
+    return typeof name === "string";
+  };
+
+  private waitForEpisodeNameToShow = async (): Promise<void> => {
+    await pWaitFor(this.getIfEpisodeNameFound, { interval: 50, timeout: 2000 });
+  };
+
+  private clickVideoAndWaitForEpisodeName = async (
+    video: HTMLVideoElement
+  ): Promise<EpisodeName> => {
+    video.click();
+    await this.waitForEpisodeNameToShow();
+    return this.findEpisodeName();
+  };
+
+  private tryGettingEpisodeName = async (): Promise<EpisodeName> => {
+    const video: PossibleVideo = this.queryVideo();
+    if (video) return this.clickVideoAndWaitForEpisodeName(video);
+    return null;
+  };
+
+  public getEpisodeInfo = async (): Promise<NetflixEpisodeInfo> => {
+    const timeInMs: PossibleEpisodeTime = this.getEpisodeTime();
+    const episodeName: EpisodeName = await this.tryGettingEpisodeName();
+    return { timeInMs, episodeName };
   };
 }
