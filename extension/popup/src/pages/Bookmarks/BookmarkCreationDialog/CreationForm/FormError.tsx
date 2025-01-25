679b5d5cc01ac40e1eef885831814c23129320cf
EDUARDO VALENCIA <8025918@gmail.com>
2025-01-24 20:04:08 -0800

    Show better error msg when disconnected

--- extension/popup/src/pages/Bookmarks/BookmarkCreationDialog/CreationForm/FormError.tsx
+++ extension/popup/src/pages/Bookmarks/BookmarkCreationDialog/CreationForm/FormError.tsx
@@ -9,11 +9,21 @@ interface Props {
 type DebugMessage = string | null;
 
 export default function FormError({ errorInfo }: Props): JSX.Element {
+  const getIfIsConnectionError = (msg: string): boolean => {
+    const expectedMsg = `Could not establish connection. Receiving end does not exist.`;
+    return msg === expectedMsg;
+  };
+
+  const createMsgFromError = ({ name, message }: Error): string => {
+    const isConnectionError: boolean = getIfIsConnectionError(message);
+    if (isConnectionError)
+      return "Sorry, there was a problem. Please try reloading the tab. If the issue persists, contact support.";
+    return `${name}: ${message}`;
+  };
+
   const getDebugErrorMessage = (): DebugMessage => {
     const { error } = errorInfo;
-    if (error instanceof Error) {
-      return `${error.name}: ${error.message}`;
-    }
+    if (error instanceof Error) return createMsgFromError(error);
     return null;
   };
 
