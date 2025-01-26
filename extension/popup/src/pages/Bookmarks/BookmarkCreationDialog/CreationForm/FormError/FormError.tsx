207dce4339ccd5890ecd9c4a75c60e0c102de3c9
EDUARDO VALENCIA <8025918@gmail.com>
2025-01-25 18:17:18 -0800

    refactor generic error

--- extension/popup/src/pages/Bookmarks/BookmarkCreationDialog/CreationForm/FormError/FormError.tsx
+++ extension/popup/src/pages/Bookmarks/BookmarkCreationDialog/CreationForm/FormError/FormError.tsx
@@ -1,13 +1,11 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
 import { FormErrorInfo } from "..";
-import { DebugInfo } from "./DebugInfo";
+import { GenericErrorWithDebugInfo } from "./GenericErrorWithDebugInfo";
 
 interface Props {
   errorInfo: FormErrorInfo;
 }
-
-type DebugMessage = string | null;
 
 export default function FormError({ errorInfo }: Props): JSX.Element {
   const getIfIsConnectionError = (): boolean => {
@@ -15,23 +13,6 @@ export default function FormError({ errorInfo }: Props): JSX.Element {
     if (!(error instanceof Error)) return false;
     const expectedMsg = `Could not establish connection. Receiving end does not exist.`;
     return error.message === expectedMsg;
-  };
-
-  const createMsgFromError = ({ name, message }: Error): string => {
-    return `${name}: ${message}`;
-  };
-
-  const getDebugErrorMessage = (): DebugMessage => {
-    const { error } = errorInfo;
-    if (error instanceof Error) return createMsgFromError(error);
-    return null;
-  };
-
-  type DebugInfo = JSX.Element | null;
-
-  const tryRenderingDebugInfo = (): DebugInfo => {
-    const debugMsg: DebugMessage = getDebugErrorMessage();
-    return debugMsg ? <DebugInfo>{debugMsg}</DebugInfo> : null;
   };
 
   const renderConnectionError = (): JSX.Element => {
@@ -43,21 +24,10 @@ export default function FormError({ errorInfo }: Props): JSX.Element {
     );
   };
 
-  const renderGenericErrorWithDebugInfo = (): JSX.Element => {
-    return (
-      <>
-        <Typography color="error">
-          Sorry, there was a problem. Please try again later.
-        </Typography>
-        {tryRenderingDebugInfo()}
-      </>
-    );
-  };
-
   const renderContent = (): JSX.Element => {
     const isConnectionError: boolean = getIfIsConnectionError();
     if (isConnectionError) return renderConnectionError();
-    return renderGenericErrorWithDebugInfo();
+    return <GenericErrorWithDebugInfo errorInfo={errorInfo} />;
   };
 
   return <Box sx={{ mb: "2rem" }}>{renderContent()}</Box>;
