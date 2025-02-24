830fda91e78b268e648eaac7d5e45ab6a82c1d55
EDUARDO VALENCIA <8025918@gmail.com>
2025-02-24 13:05:10 -0800

    Add icon to save btn

--- extension/popup/src/pages/Bookmarks/BookmarkCreationDialog/CreationForm/FormButtonsToolbar/SaveButton.tsx
+++ extension/popup/src/pages/Bookmarks/BookmarkCreationDialog/CreationForm/FormButtonsToolbar/SaveButton.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import FormActionButton from "../FormActionButton";
+import Check from "@mui/icons-material/Check";
 
 export default function SaveButton() {
   return (
-    <FormActionButton type="submit" variant="contained" color="primary">
+    <FormActionButton
+      type="submit"
+      variant="contained"
+      color="primary"
+      endIcon={<Check />}
+    >
       Save
     </FormActionButton>
   );
