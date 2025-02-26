056a27bf847f7ea0618e12abb57530aef4f90bf2
EDUARDO VALENCIA <8025918@gmail.com>
2025-02-26 12:28:24 -0800

    Doc injection results

--- extension/popup/src/scripts/Scripts.repo.ts
+++ extension/popup/src/scripts/Scripts.repo.ts
@@ -4,5 +4,8 @@ import { ScriptsRepoAbstraction } from "./Scripts.repo-abstraction";
 export class ScriptsRepo extends ScriptsRepoAbstraction {
   private chrome = getChrome();
 
+  /**
+   * For info about injection results, see https://developer.chrome.com/blog/crx-scripting-api#script_injection_results
+   */
   public executeScript = this.chrome.scripting.executeScript;
 }
