1fe7a2866c0849055f9fcf69578066a3e1e75754
EDUARDO VALENCIA <8025918@gmail.com>
2025-03-03 19:06:35 -0800

    Add eslint-plugin-import

--- extension/popup/.eslintrc.js
+++ extension/popup/.eslintrc.js
@@ -4,14 +4,21 @@ module.exports = {
     "react-app/jest",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:import/recommended",
   ],
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "eslint-plugin-import"],
   parserOptions: {
     project: true,
     tsconfigRootDir: __dirname,
   },
   rules: {
     "@typescript-eslint/no-unsafe-assignment": "warn",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
   },
 };
